58c72e06d4d9cb912af77068e69ad252450bbbf1
Wictronix <111145579+Wictronix@users.noreply.github.com>
2022-08-28 22:58:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Origami
+A Stationery Website! Made with Html, Tailwind Css & JavaScript.
+
+
+![Screenshot from 2022-07-16 06-27-11](https://user-images.githubusercontent.com/78796441/179328086-524766db-cacc-450a-b263-ac03e760a148.png)
+
+
+Enjoy!
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
